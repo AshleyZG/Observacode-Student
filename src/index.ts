2ee6918be4870bed46b7bb0f377b54f1e6f6dad3
AshleyZG <gsarazh@gmail.com>
2022-05-21 22:34:39 -0400

    init extension

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the observacode_student extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'observacode_student:plugin',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension observacode_student is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('observacode_student settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for observacode_student.', reason);
+        });
+    }
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The observacode_student server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
